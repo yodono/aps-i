7fb4b7fdf86edb2fa30b1f903d7e160858d7dad0
yodono-unip <victor.yodono@aluno.unip.br>
2022-05-01 20:20:47 +0000

    feat(components): Adiciona componente de rodapé

---
+++ components/footer.js
@@ -0,0 +1,36 @@
+function renderFooter() {
+    document.getElementById("footer").innerHTML += `
+        <div>
+            <strong>Social</strong>
+            <ul>
+                <li><a href="contato.html">Contate-nos</a></li>
+                <li><a href="sobre.html">Sobre</a></li>
+                <li><a href="https://github.com/yodono-unip/aps-i" rel="noopener" target="_blank">Github</a></li>
+            </ul>
+        </div>
+
+        <div>
+            <strong>Problemas</strong>
+            <ul>
+                <li><a href="problemas/amazonia.html">Amazônia</a></li>
+                <li><a href="problemas/hidreletricas.html">Hidrelétricas</a></li>
+                <li><a href="problemas/pecuaria.html">Pecuária</a></li>
+            </ul>
+        </div>
+
+        <div>
+            <strong>Alternativas</strong>
+            <ul>
+                <li><a href="alternativas/eolica.html">Energia Eólica</a></li>
+                <li><a href="alternativas/solar.html">Energia Solar</a></li>
+                <li><a href="alternativas/maremotriz.html">Energia Maremotriz</a></li>
+            </ul>
+        </div>
+
+        <div>
+            <span>Cabral, Belchior, Brito, Diniz, Masson e Yodono - Tópicos de Meio Ambiente Nacional @ 2022</span>
+        </div>
+    ` 
+};
+
+renderFooter();
