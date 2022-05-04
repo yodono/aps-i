ce3a05d7a705e3de4c50630d02eb7659dc6918f5
LHMasson <lucas.masson001@gmail.com>
2022-05-04 14:13:26 -0300

    Finaliza e Estiliza NavBar

--- components/navBar.js
+++ components/navBar.js
@@ -1,59 +1,29 @@
 function rendernavBar(){
     document.getElementById("navBar").innerHTML += /* html */` 
-        <div class="sidebar">
-        <div class="logo_content">
-            <div class="logo">
-            <i class="fa-solid fa-dove"></i>
-                <div class="logo_name">ToMAN</div>
-            </div>
-            <i class="fa-solid fa-bars" id="btn"></i>
-        </div>  
-        <ul class="nav_list">
-            <li><a href="#">
-                <div class="icon-wrapper"><i class="fa-solid fa-house"></i></div>
-                <span class="links_name">Home</span>
-            </a></li>
-            <!-- <span class="tooltip">Home</span> -->
-            
-            <li><a href="#">
-            <div class="icon-wrapper"><i class="fa-solid fa-tree"></i></div>
-                <span class="links_name">Amazônia</span>
-            </a></li>
-            <!-- <span class="tooltip">Amazônia</span> -->
-
-            <li><a href="#">
-            <div class="icon-wrapper"><i class="fa-solid fa-water"></i></div>
-                <span class="links_name">Hidrelétricas</span>
-            </a></li>
-            <!-- <span class="tooltip">Hidrelétricas</span> -->
-
-            <li><a href="#">
-            <div class="icon-wrapper"><i class="fa-solid fa-cow"></i></div>
-                <span class="links_name">Pecuária</span>
-            </a></li>
-            <!-- <span class="tooltip">Pecuária</span> -->
-
-            <li><a href="#">
-            <div class="icon-wrapper"><i class="fa-solid fa-fan"></i></div>
-                <span class="links_name">Energia Eólica</span>
-            </a></li>
-            <!-- <span class="tooltip">Energia Eólica</span> -->
-
-            <li><a href="#">
-            <div class="icon-wrapper"><i class="fa-solid fa-sun"></i></div>
-                <span class="links_name">Energia Solar</span>
-            </a></li>
-            <!-- <span class="tooltip">Energia Solar</span> -->
-
-            <li><a href="#">
-                <div class="icon-wrapper"><i class="fa-solid fa-fish"></i></div>
-                <span class="links_name">Energia Maremotriz</span>
-            </a></li>
-            <!-- <span class="tooltip">Energia Maremotriz</span> -->
-
-            </ul>
+    <nav>   
+        <div class="logo">
+            <h4>ToMAN</h4>
         </div>
-  
+        <ul class="nav_links">
+            <li>
+                <a href="#">Home</a>
+            </li>
+            <li>
+                <a href="#">Sobre</a>
+            </li>
+            <li>
+                <a href="#">Problemas</a>
+            </li>
+            <li>
+                <a href="#">Alternativas</a>
+            </li>
+        </ul>
+        <div class="burger">
+            <div class="line1"></div>
+            <div class="line2"></div>
+            <div class="line3"></div>
+        </div>
+    </nav>
     `
 };
 
---
+++ components/navSlide.js
@@ -0,0 +1,25 @@
+const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav_links');
+    const navLinks = document.querySelectorAll('.nav_links li');
+
+    // Toggle Nav
+    burger.addEventListener('click',()=>{
+        // Toggle Nav
+        nav.classList.toggle('nav-active');
+    
+        // Animate Links
+        navLinks.forEach((link, index) => {
+            if (link.style.animation) {
+                link.style.animation = '';
+            } else {
+            link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`;
+            }
+        });
+
+        // Burger Animation
+        burger.classList.toggle('toggle');
+    });
+}
+
+navSlide()
