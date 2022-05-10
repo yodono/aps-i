0560e0241186bb0c493277d210cae31ff7ac6a66
Brain-play1 <leonardo.dinizbassiilio@gmail.com>
2022-05-10 18:18:59 +0000

    Carrossel 2 - 10/05
    Signed-off-by: Brain-play1 <leonardo.dinizbassiilio@gmail.com>

--- components/carrossel.js
+++ components/carrossel.js
@@ -1,26 +1,19 @@
-let slideIndex = 1;
-        showSlides(slideIndex);
-        
-        function proSlide(n) {
-          showSlides(slideIndex += n);
-        }
-        
-        function slideAtual(n) {
-          showSlides(slideIndex = n);
-        }
-        
-        function showSlides(n) {
-          let i;
-          let slides = document.getElementsByClassName("mySlides");
-          let dots = document.getElementsByClassName("dot");
-          if (n > slides.length) {slideIndex = 1}    
-          if (n < 1) {slideIndex = slides.length}
-          for (i = 0; i < slides.length; i++) {
-            slides[i].style.display = "none";  
-          }
-          for (i = 0; i < dots.length; i++) {
-            dots[i].className = dots[i].className.replace(" active", "");
-          }
-          slides[slideIndex-1].style.display = "block";  
-          dots[slideIndex-1].className += " active";
-        }
+let slideIndex = [1,1];
+let slideId = ["meusSlides1", "meusSlides2"]
+showSlides(1, 0);
+showSlides(1, 1);
+
+function proSlides(n, no) {
+  showSlides(slideIndex[no] += n, no);
+}
+
+function showSlides(n, no) {
+  let i;
+  let x = document.getElementsByClassName(slideId[no]);
+  if (n > x.length) {slideIndex[no] = 1}    
+  if (n < 1) {slideIndex[no] = x.length}
+  for (i = 0; i < x.length; i++) {
+     x[i].style.display = "none";  
+  }
+  x[slideIndex[no]-1].style.display = "block";  
+}
