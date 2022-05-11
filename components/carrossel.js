027c81eadb3af11e1e568e3426d8b26cebee0139
Brain-play1 <leonardo.dinizbassiilio@gmail.com>
2022-05-11 17:34:45 +0000

    Commit 11/05
    Signed-off-by: Brain-play1 <leonardo.dinizbassiilio@gmail.com>

--- components/carrossel.js
+++ components/carrossel.js
@@ -15,5 +15,5 @@ function showSlides(n, no) {
   for (i = 0; i < x.length; i++) {
      x[i].style.display = "none";  
   }
-  x[slideIndex[no]-1].style.display = "block";  
+  x[slideIndex[no]-1].style.display = "block"; 
 }
