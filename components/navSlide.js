10a6fda303e1e23f3280163d52c1a23c87037fd1
yodono-unip <victor.yodono@aluno.unip.br>
2022-05-15 22:58:28 +0000

    fix: Arruma animação indesejada após redimensionamento

--- components/navSlide.js
+++ components/navSlide.js
@@ -1,3 +1,5 @@
+const MOBILE_WIDTH_PX = 768;
+
 const navSlide = () => {
     const burger = document.querySelector('.burger');
     const nav = document.querySelector('.nav_links');
@@ -10,7 +12,7 @@ const navSlide = () => {
     
         // Animate Links
         navLinks.forEach((link, index) => {
-            if (link.style.animation) 
+            if (link.style.animation || window.screen.width < MOBILE_WIDTH_PX) 
                 link.style.animation = '';
             else link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`; 
         });
